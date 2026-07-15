890bd952f5775b3d6f08ccd701c4a455c31a45d2
Igor Hendrix <igorhendrix11@gmail.com>
2026-07-14 22:43:00 -0300

    First steps of main.cpp
    Greetins to the user and question if they already hava an account

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Workout.hpp"
+
+
+
+int main() {
+    Workout wkt;
+ 
+    cout << "\t |  Welcome to 2T - TRAINING TIME  |\n\n" << endl;
+    cout << "A place where you can be happy doing your workout and respecting the time you have!\n" << endl;
+
+    cout << "Please, tell me if you already have an account:\n[1]Yes\n[2]No\nAnswer: " << endl;
+    int a; // 'a' used to know if the user has an account or not
+    cin >> a;
+
+
+
+
+
+
+    return 0;
+}
